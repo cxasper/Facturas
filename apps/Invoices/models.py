5f0933cf865070edac49c680a9047d5737529cb1
Andersson Velasquez Alvites <cxasper23@gmail.com>
2019-05-17 19:39:37 -0500

    add null total invoice

--- apps/Invoices/models.py
+++ apps/Invoices/models.py
@@ -23,7 +23,7 @@ class Invoice(models.Model):
     number = models.IntegerField(
         validators=[MinValueValidator(0)])
 
-    total = models.CharField(max_length=55)
+    total = models.CharField(max_length=55, null=True)
 
     def __str__(self):
         return str(self.id)
